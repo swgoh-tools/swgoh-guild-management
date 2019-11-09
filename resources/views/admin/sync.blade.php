c7beed5a89c5fe9283db11c81a67e3d948951c87
VKSG <vksg@galaxy.ovh>
2019-11-09 16:55:31 +0100

    show pending and failed background jobs on sync page

--- resources/views/admin/sync.blade.php
+++ resources/views/admin/sync.blade.php
@@ -14,52 +14,82 @@
     </div>
 
     <div class="row">
-        <p>{{ __('pages.sync.intro') }}</p>
-        <p>{{ __('pages.sync.description') }}</p>
-        <p>{{ __('pages.sync.legend') }}</p>
-        <p>{{ __('app.report_any_issues') }}</p>
+        <div class="col-md-7">
+            <p>{{ __('pages.sync.intro') }}</p>
+            <p>{{ __('pages.sync.description') }}</p>
+            <p>{{ __('pages.sync.legend') }}</p>
+            <p>{{ __('app.report_any_issues') }}</p>
+        </div>
+        <div class="col-md-5 page-action text-right">
+            <ul class="list-group">
+                <li class="list-group-item d-flex justify-content-between align-items-center">{{ __('Pending Email') }}
+                    <span class="badge badge-primary badge-pill">{{ Queue::size('email') }}</span></li>
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    {{ __('Pending Guild Sync') }}
+                    <span class="badge badge-primary badge-pill">{{ Queue::size('default') }}</span></li>
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    {{ __('Pending Data Sync') }}
+                    <span class="badge badge-primary badge-pill">{{ Queue::size('low') }}</span></li>
+                <li class="list-group-item d-flex justify-content-between align-items-center">{{ __('Total') }}
+                    <span class="badge badge-secondary badge-pill">{{ Queue::size() }}</span></li>
+                <li class="list-group-item d-flex justify-content-between align-items-center">{{ __('Failed') }}
+                    <span class="badge badge-danger badge-pill">{{ DB::table('failed_jobs')->count() }}</span></li>
+            </ul>
+        </div>
     </div>
 
     <div class="row">
         <table class="table table-bordered table-striped table-hover">
             <thead>
-            <tr>
-                <th>{{ __('No') }}</th>
-                <th>{{ __('Target') }}</th>
-                <th>{{ __('Last Run') }}</th>
-                <th>{{ __('State') }}</th>
-                <th>{{ __('Currentness') }}</th>
-                <th>{{ __('Size') }}</th>
-                <th>{{ __('Details') }}</th>
-                @hasanyrole('admin|leader|officer|member')
-                <th class="text-center" style="min-width:4.5rem;">{{ __('Actions') }}</th>
-                @endhasanyrole
-            </tr>
+                <tr>
+                    <th>{{ __('No') }}</th>
+                    <th>{{ __('Target') }}</th>
+                    <th>{{ __('Last Run') }}</th>
+                    <th>{{ __('State') }}</th>
+                    <th>{{ __('Currentness') }}</th>
+                    <th>{{ __('Size') }}</th>
+                    <th>{{ __('Details') }}</th>
+                    @hasanyrole('admin|leader|officer|member')
+                    <th class="text-center" style="min-width:4.5rem;">{{ __('Actions') }}</th>
+                    @endhasanyrole
+                </tr>
             </thead>
             <tbody>
-            @foreach($targets as $key => $description)
+                @foreach($targets as $key => $description)
                 <tr>
                     <td>{{ $loop->iteration }}</td>
                     <td>{{ $description ?: $key }}</td>
-                        @if( ! isset($sync_status[$key]) || ! is_array($sync_status[$key]))
-                            <td>-</td><td>-</td><td>-</td><td>-</td><td>{{ $sync_status[$key] ?? '-' }}</td>
-                        @else
-                            <td>{{ isset($sync_status[$key]['time']) ? gmdate("d.m.y H:i", $sync_status[$key]['time']) : '-' }}</td>
-                            <td>{{ $sync_status[$key]['status'] ?? '-' }}</td>
-                            <td>{{ isset($sync_status[$key]['time_data']) ? gmdate("d.m.y H:i", $sync_status[$key]['time_data']) : '-' }}</td>
-                            <td class="text-right">{{ isset($sync_status[$key]['size']) && is_int($sync_status[$key]['size']) ? number_format($sync_status[$key]['size']) : '-' }}</td>
-                            <td>{{ $sync_status[$key]['url'] ?? '-' }}
-                            @role('admin')
-                                <br />{{ isset($sync_status[$key]['error']) ? $sync_status[$key]['error'] : '-' }}
-                                {{ ! isset($sync_status[$key]['time']) ? var_dump($sync_status[$key]) : '' }}
-                                @if(isset($sync_status[$key.'PEEK']) && is_array($sync_status[$key.'PEEK']))<br />@foreach($sync_status[$key.'PEEK'] as $peek_key => $peek_value) {{$peek_key . ': ' . $peek_value . '; ' }} @endforeach @endif
-                                @if(isset($sync_status[$key.'STRUCTURE']) && is_array($sync_status[$key.'STRUCTURE']))<br />@foreach($sync_status[$key.'STRUCTURE'] as $peek_key => $peek_value) {{$peek_key . ': ' . $peek_value . '; ' }} @endforeach @endif
-                            @endrole
-                            </td>
-                        @endif
+                    @if( ! isset($sync_status[$key]) || ! is_array($sync_status[$key]))
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>{{ $sync_status[$key] ?? '-' }}</td>
+                    @else
+                    <td>{{ isset($sync_status[$key]['time']) ? gmdate("d.m.y H:i", $sync_status[$key]['time']) : '-' }}
+                    </td>
+                    <td>{{ $sync_status[$key]['status'] ?? '-' }}</td>
+                    <td>{{ isset($sync_status[$key]['time_data']) ? gmdate("d.m.y H:i", $sync_status[$key]['time_data']) : '-' }}
+                    </td>
+                    <td class="text-right">
+                        {{ isset($sync_status[$key]['size']) && is_int($sync_status[$key]['size']) ? number_format($sync_status[$key]['size']) : '-' }}
+                    </td>
+                    <td>{{ $sync_status[$key]['url'] ?? '-' }}
+                        @role('admin')
+                        <br />{{ isset($sync_status[$key]['error']) ? $sync_status[$key]['error'] : '-' }}
+                        {{ ! isset($sync_status[$key]['time']) ? var_dump($sync_status[$key]) : '' }}
+                        @if(isset($sync_status[$key.'PEEK']) &&
+                        is_array($sync_status[$key.'PEEK']))<br />@foreach($sync_status[$key.'PEEK'] as $peek_key =>
+                        $peek_value) {{$peek_key . ': ' . $peek_value . '; ' }} @endforeach @endif
+                        @if(isset($sync_status[$key.'STRUCTURE']) &&
+                        is_array($sync_status[$key.'STRUCTURE']))<br />@foreach($sync_status[$key.'STRUCTURE'] as
+                        $peek_key => $peek_value) {{$peek_key . ': ' . $peek_value . '; ' }} @endforeach @endif
+                        @endrole
+                    </td>
+                    @endif
                     @hasanyrole('admin|leader|officer|member')
                     <td class="text-center">
-                    {!! Form::open( ['method' => 'post', 'url' => route('sync'), 'style' => 'display: inline']) !!}
+                        {!! Form::open( ['method' => 'post', 'url' => route('sync'), 'style' => 'display: inline']) !!}
                         {!! Form::hidden('sync', $key) !!}
                         @if($key == 'clear')
                         @role('admin')
@@ -83,30 +113,30 @@
                         </button>
                         @endrole
                         @endif
-                    {!! Form::close() !!}
+                        {!! Form::close() !!}
                     </td>
                     @endhasanyrole
                 </tr>
-            @endforeach
+                @endforeach
             </tbody>
         </table>
 
         <div class="text-left">{{ __('Server Time') }} ({{ date("e", time()) }})
             <ul>
-            <li class="">{{ gmdate("d.m.y H:i", time()) }}</li>
-            <li class="">{{ date("c", time()) }}</li>
-            <li class="">{{ date("r", time()) }}</li>
+                <li class="">{{ gmdate("d.m.y H:i", time()) }}</li>
+                <li class="">{{ date("c", time()) }}</li>
+                <li class="">{{ date("r", time()) }}</li>
             </ul>
         </div>
 
         <div class="text-left">
-        @role('admin')
+            @role('admin')
             @if(isset($sync_status['error']) && is_array($sync_status['error']))
-                {{ implode(', ', $sync_status['error']) }}
+            {{ implode(', ', $sync_status['error']) }}
             @else
-                {{ $sync_status['error'] ?? '-' }}
+            {{ $sync_status['error'] ?? '-' }}
             @endif
-        @endrole
+            @endrole
         </div>
     </div>
 </div>
